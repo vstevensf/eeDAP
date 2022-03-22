bbb650252e6846a1eed2bee8b51e3e823c6bb576
Stevens Victoria <vs384@zoo.cs.yale.edu>
2022-03-22 16:49:43 -0400

    Annotation of GetExecutableFolder() function
    Sets executable folder based on whether the user is running in standalone mode (from eeDAP installation directly)
    or running the MATLAB files directly (in MATLAB integrated development environment).

--- src/GetExecutableFolder.m
+++ src/GetExecutableFolder.m
@@ -1,17 +1,24 @@
+% this function is used in Administrator_Input_screen.m and within camera stage review
 function [executableFolder] = GetExecutableFolder() 
 try
     if isdeployed 
 			% User is running an executable in standalone mode. 
+            % calls upon OS to run "set PATH" as environment when downloaded eeDAP.exe file
 			[status, result] = system('set PATH');
+
+            % match regex case insensitive
 			executableFolder = char(regexpi(result, 'Path=(.*?);', 'tokens', 'once'));
 % 			fprintf(1, '\nIn function GetExecutableFolder(), currentWorkingDirectory = %s\n', executableFolder);
     else
 			% User is running an m-file from the MATLAB integrated development environment (regular MATLAB).
 			executableFolder = pwd; 
     end
+% MException object: includes stack trace information and error message
 catch ME
      errorMessage = sprintf('Error in function %s() at line %d.\n\nError Message:\n%s', ...
                 ME.stack(1).name, ME.stack(1).line, ME.message);
+
+    % blocks execution and creates warning dialog box
      uiwait(warndlg(errorMessage));
 end
 return;
