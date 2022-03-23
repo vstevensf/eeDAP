1b584691ba233cf67553ff03e134b25211f7acac
Stevens Victoria <vs384@zoo.cs.yale.edu>
2022-03-22 22:52:41 -0400

    Added annotations for self in is_octave.m

--- src/bfmatlab/private/is_octave.m
+++ src/bfmatlab/private/is_octave.m
@@ -1,3 +1,5 @@
+% GNU octave is a scientific programming language used for plotting and visualization tools
+% It is also MATLAB compatible
 function is = is_octave ()
 is = exist ('OCTAVE_VERSION', 'builtin') == 5;
 end
