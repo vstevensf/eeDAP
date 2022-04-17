e2fddf99b7d8a3a0fdd18b94fd8481e5f29e36ab
Stevens Victoria <vs384@zoo.cs.yale.edu>
2022-04-17 01:02:36 -0400

    added comments about camera_open function

--- src/camera_open.m
+++ src/camera_open.m
@@ -1,6 +1,13 @@
-
+% first called from function StartTheTestButtonPressed(hObject, eventdata, handles)
+% within src/Administrator_Input_Screen.m
+% also called within src/GUI.m, src/Stage_Allighment.m,...
+% utilities/Camera_stage_review/Camera_stage_review.m, utilities/Camera_stage_review/camera_open.m
 function cam=camera_open(cam_kind,cam_format,defaultR,defaultB)
+% returns a structure populated with info based on the camera type
+% this is saved in the MyData structure described in Administrator_Input_Screen.m and GUI.m
 try
+
+    % the following are from Matlab's image acquisition toolbox
 
     % imaqtool: launches an interactive GUI to allow you to explore,
     %   configure, and acquire data from your installed and supported image
@@ -8,7 +15,7 @@ try
     
     % imaqhwinfo: a structure that contains information about the image
     %   acquisition adaptors available on the system. An adaptor is the
-    %   interface between MATLAB® and the image acquisition devices
+    %   interface between MATLABï¿½ and the image acquisition devices
     %   connected to the system. The adaptor's main purpose is to pass
     %   information between MATLAB and an image acquisition device via its
     %   driver.
@@ -25,18 +32,49 @@ try
     % delete any currently running (stale) video inputs
     objects = imaqfind;
     delete(objects);
+    % if type of camera is USB
     if strcmp( cam_kind,'USB')
        cam_adaptor = 'pointgrey';
+    % if the camera is connected by Firewire (an Apple version of IEEE 1394 interface that allows high-speed data transferring between devices)
     elseif strcmp( cam_kind,'Firewire')
        cam_adaptor = 'dcam';
     end
+
     % Create the video object to communicate with the camera 
+    % this is the live display of what's in the "camera's eye" on the computer screen
+    % checks if "cam_format" is an existing variable in the workspace (return = 1 if exists)
+    % cam_format is a variable defined in the dapsi file
     if exist('cam_format','var')
+        % A videoinput object represents a connection between MATLABÂ® and a particular image acquisition device.
+        % vid = videoinput(adaptor,deviceID,format)
+        % vid = videoinput(adaptor,deviceID,format) creates a video input object vid, 
+        % where format is a character vector that specifies a particular video format supported 
+        % by the device or the full path of a device configuration file (also known as a camera file). 
+        % To get a list of the formats supported by a particular device, view the DeviceInfo structure for the device 
+        % that is returned by imaqhwinfo. Each DeviceInfo structure contains a SupportedFormats field. 
+        % If format is not specified, the device's default format is used. 
+        % When the video input object is created, its VideoFormat property contains the format name or device configuration file that you specify.
         cam = videoinput(cam_adaptor,1,cam_format) %#ok<NOPRT>
     else
+        % vid = videoinput(adaptor,deviceID)
+        % creates a video input object vid, where deviceID is a numeric scalar value that identifies a particular device available 
+        % through the specified adaptor, adaptor. Use imaqhwinfo(adaptor) to determine the devices available through the specified adaptor. 
+        % If deviceID is not specified, the first available device ID is used. You can also use a device's name in place of deviceID. 
+        % If multiple devices have the same name, the first available device is used.
         cam = videoinput(cam_adaptor,1) %#ok<NOPRT>
     end
+
     imaqhwinfo(cam)
+    % This example returns information about all the devices accessible through a particular adaptor.
+    % info = imaqhwinfo('winvideo')
+    % info = 
+    %     AdaptorDllName: [1x73 char]
+    %     AdaptorDllVersion: '2.1 (R2007a)'
+    %     AdaptorName: 'winvideo'
+    %     DeviceIDs: {[1]}
+    %     DeviceInfo: [1x1 struct]
+
+    % adding more attributes to the struct with info about this type of camera adaptor (USB vs Firewire)
     cam.Tag = 'Microscope Camera Object';
     cam.TriggerRepeat = 0;
     cam.FramesPerTrigger = 10;
@@ -50,6 +88,7 @@ try
         uiwait(h_errordlg)
         close all force
     end
+
     % Camera image must be rgb
     if ~strcmp(cam.ReturnedColorSpace,'rgb');
         desc = 'Camera image must be rgb' %#ok<NOPRT>
@@ -59,7 +98,7 @@ try
     end
     
     % Set value of a video source object property.
-    cam_src = getselectedsource(cam);
+    cam_src = getselectedsource(cam); % Return currently selected video source object
     cam_src.Tag = 'Microscope Camera Source';
     if strcmp( cam_kind,'USB')
        cam_src.WhiteBalanceRBMode = 'Off';
